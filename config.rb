e30adc05be1709c3a63e02b9554e53e9eb1f74e0
yoshimitsu <yoshimitsu@g-hill.jp>
2020-08-10 19:29:49 +0900

    first commit

---
+++ config.rb
@@ -0,0 +1,10 @@
+# Set this to the root of your project when deployed:
+http_path = "/" 
+css_dir = "/"
+sass_dir = "/"
+images_dir = "images"
+javascripts_dir = "js"
+output_style = "expanded"
+line_comments = false
+relative_assets = true
+Encoding.default_external = "utf-8"
